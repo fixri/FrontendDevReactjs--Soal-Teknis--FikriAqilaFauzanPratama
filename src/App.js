3f4a8782834981ab1ca19173bbd9ad5bdc809195
FikriAqila <107942065+fixri@users.noreply.github.com>
2022-06-26 20:56:21 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './form';
+import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
+
+
+
+function App() {
+  return (
+      <div className="wrapper">
+    <Container>
+        <h1>Restaurants</h1>
+        <br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis arcu quis sapien aliquam, et porta orci feugiat. Duis ut vestibulum leo, eget blandit metus. Curabitur volutpat massa felis</p>
+        <hr></hr> 
+        
+        <p>Filter by :</p> 
+        
+        <hr></hr> 
+
+
+
+</Container>
+
+     </div>
+    
+  
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
